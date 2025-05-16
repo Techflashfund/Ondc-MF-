8a6b02dde562c2a4e41b4ecd69921b1907f972f9
amalnath369 <amalnaths640@gmail.com>
2025-05-16 12:58:25 +0530

    select submit digi

--- ondc/urls.py
+++ ondc/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('on_confirm',OnConfirmSIP.as_view(),name='on_confirm'),
     path('on_status',OnStatusView.as_view(),name='on_status'),
     path('on_update',OnUpdateView.as_view(),name='on_update'),
-    path('digisend',DigiLockerFormSubmission.as_view(),name='digisend')
+    path('digisend',DigiLockerFormSubmission.as_view(),name='digisend'),
+    path('esignsub',EsignFormSubmission.as_view(),name='esignsub'),
 ]
 
