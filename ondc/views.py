a2cefc88b489e597396ac8f5e6a2b0e550d4e62c
Amalnath <amalnaths640@gmail.com>
2025-05-19 21:43:55 +0530

    lumpsum exixting init

--- ondc/views.py
+++ ondc/views.py
@@ -2977,7 +2977,7 @@ class LumpsumExistingFolioInit(APIView):
             "X-Gateway-Subscriber-Id": os.getenv("SUBSCRIBER_ID")
         }
 
-        response = requests.post(f"{bpp_uri}/select", data=request_body_str, headers=headers) 
+        response = requests.post(f"{bpp_uri}/init", data=request_body_str, headers=headers) 
         return Response({
                 "status_code": response.status_code,
                 "response": response.json() if response.content else {}
