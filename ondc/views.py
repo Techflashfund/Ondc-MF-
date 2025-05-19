bc11dc6d3169a4e4b3429d96c3506e41748a1772
amalnath369 <amalnaths640@gmail.com>
2025-05-19 16:56:38 +0530

    Lumpsum With KYC

--- ondc/views.py
+++ ondc/views.py
@@ -2633,7 +2633,7 @@ class LumpsumDigiLockerSubmission(APIView):
             "X-Gateway-Subscriber-Id": os.getenv("SUBSCRIBER_ID")
         }
 
-        response = requests.post(f"{bpp_uri}/confirm", data=request_body_str, headers=headers) 
+        response = requests.post(f"{bpp_uri}/select", data=request_body_str, headers=headers) 
         return Response({
                 "status_code": response.status_code,
                 "response": response.json() if response.content else {}
