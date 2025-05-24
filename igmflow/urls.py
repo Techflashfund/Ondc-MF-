e37366248915da8f974de44cc2245bfe64380f64
amalnath369 <amalnaths640@gmail.com>
2025-05-24 12:43:02 +0530

    updaed

--- igmflow/urls.py
+++ igmflow/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns=[
     path('issue/',IGMIssue.as_view(),name='issue'),
     path('on_issue',OnIssueView.as_view(),name='on_issue'),
+
+    path('issue_status',IssueStatusView.as_view(),name='issue_status'),
+    path('on_issue_status',OnIssueStatusView.as_view(),name='on_issue_status'),
 ]
